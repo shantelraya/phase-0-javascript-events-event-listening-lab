103ef7d6972527d5733266520d4ab125a1150b9d
raya <parischax@gmail.com>
2022-08-11 23:59:11 +0300

    done assignments

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    input.addEventListener('click', function() {
+        alert("binds an event listener in addingEventListener()")
+    });
 }
+addingEventListener(); 
